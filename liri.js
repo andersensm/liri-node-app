4c452edb19352a432793de014a387f3e05b0be70
Sean Andersen <smandersen13@gmail.com>
2018-02-03 12:15:13 -0500

    added 20 most recent tweets

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+var arguments = process.argv
+
+console.log(arguments)
+
 require("dotenv").config();
 
 var keys = require("./keys.js")
@@ -7,6 +11,11 @@ var Twitter = require('twitter')
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+if (arguments[2] === 'my-tweets'){
+  client.get('statuses/home_timeline', count = 20, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  })
 
-console.log(spotify)
-console.log(client)
+}
