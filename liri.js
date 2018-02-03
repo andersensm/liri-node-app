10a5a32d38df9afd0f8938e4ca8f0601222dc655
Sean Andersen <smandersen13@gmail.com>
2018-02-02 20:07:29 -0500

    liri app init

---
+++ liri.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+console.log(process.env.TWITTER_CONSUMER_KEY)
+
+console.log(process.env.TWITTER_CONSUMER_SECRET)
+
+
+* `my-tweets`
+
+* `spotify-this-song`
+
+* `movie-this`
+
+* `do-what-it-says`
